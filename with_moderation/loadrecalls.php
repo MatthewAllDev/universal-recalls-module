0e092df518b860b2fec809d14a09427b08e9abb4
MatthewAllDev <MatthewAllDev@users.noreply.github.com>
2019-02-14 11:16:43 +0300

    add load recall module and page

---
+++ with_moderation/loadrecalls.php
@@ -0,0 +1,71 @@
+<?php
+	$___notjson=1;
+	header('Content-Type: text/html; charset=utf-8');
+	setlocale(LC_ALL, 'ru_RU.65001', 'rus_RUS.65001', 'Russian_Russia. 65001', 'russian');
+	error_reporting(0);
+	
+	function search_page($data, $id)
+	{
+		foreach ($data as $page) {
+			if ($page['id'] == $id)
+				return $page;
+		}
+	}
+
+	function get_page_count($data)
+	{
+		$i = 0;
+		foreach ($data as $page) {
+			$i++;
+		}
+		return $i;
+	}
+
+	function load_recalls()
+	{
+		$xml = simplexml_load_file('recalls.xml');
+		$hr = '<hr />';
+		$page_count = get_page_count($xml);
+		if ($page_count > 1)
+		{
+			$page_selector = '<div align="center" class="otstup"><span class="swchItemA" page="1" onclick="load_page(this)">1</span>';
+			for ($i=1; $i < $page_count; $i++)
+			{
+				$page_selector = $page_selector . '<span class="swchItem" page="' . ($i + 1) . '" onclick="load_page(this)">' . ($i + 1) . '</span>';
+			} 
+			$page_selector = $page_selector . '</div>';
+		}
+		else
+			$page_selector = '';
+		$page = '<div id="recall_page">' . load_page(1, $xml) . '</div>';
+		$response = $hr . $page_selector . $page . $page_selector . $hr;
+		return $response;
+	}
+
+	function load_page($page_id, $xml=null)
+	{
+		if(!$xml)
+			$xml = simplexml_load_file('recalls.xml');
+		$elements = search_page($xml, $page_id); 
+		$response = '<hr />';
+		foreach ($elements as $recall) {
+			if ($recall->id)
+				if ($recall->date)
+					$response = $response . '<div class="recall" id="'. $recall->id .'"><div class="recall_date">' . $recall->date . '</div><div class="recall_name">' . $recall->name . ':</div><div class="recall_text">' . $recall->text . '</div></div><hr />';
+				else
+				$response = $response . '<div class="recall" id="'. $recall->id .'"><div class="recall_name">' . $recall->name . ':</div><div class="recall_text">' . $recall->text . '</div></div><hr />';
+		}
+		return $response;
+	}
+
+	if ($_GET["method"] == 'load_recalls')
+	{
+		echo load_recalls();
+	}
+
+	if ($_GET["method"] == 'load_page')
+	{
+		echo load_page($_GET['page_id']);
+	}
+	
+?>
---
+++ with_moderation/recalls_page.php
@@ -0,0 +1,97 @@
+<?php
+	$___notjson=1;
+	header('Content-Type: text/html; charset=utf-8');
+	setlocale(LC_ALL, 'ru_RU.65001', 'rus_RUS.65001', 'Russian_Russia. 65001', 'russian');
+	error_reporting(0);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<style>
+	.swchItem {
+	    color: #000;
+	    border: 1px solid #FFBD85;
+	    background: #FFFAF7;
+	}
+	.swchItemA, .swchItem:hover {
+	    font-weight: normal;
+	    color: #fff;
+	    text-shadow: 0px 1px #CA470E;
+	    box-shadow: 0px 1px #EDEDED;
+	    -webkit-box-shadow: 0px 1px #EDEDED;
+	    -moz-box-shadow: 0px 1px #EDEDED;
+	    border: 1px solid #D13F11;
+	    background: #E95B2B;
+	    background: -moz-linear-gradient(top,#FFBE01 1px,#FE7C02 1px,#E95B2B);
+	    background: -webkit-gradient(linear,0 0,0 100%,color-stop(0.02,#FFBE01),color-stop(0.02,#FE7C02),color-stop(1,#E95B2B));
+	}
+	.swchItemA, .swchItem {
+	    -moz-border-radius: 3px;
+	    -webkit-border-radius: 3px;
+	    border-radius: 3px;
+	    padding: 6px 9px;
+	    margin-left: 3px;
+	    text-decoration: none;
+	    cursor: pointer;
+	}
+	.otziv {
+	    padding: 5 8px;
+	    font: bold 13px/16px 'Century Gothic';
+	    color: #2d1f0b;
+	    border: 1px solid #756859;
+	    border-left: none;
+	    border-top: none;
+	    border-radius: 5px;
+	    background: linear-gradient(to bottom,#f9d401, #e87b00);
+	    cursor: pointer;
+	}
+	</style>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script type="text/javascript">
+		function load_recalls() {
+		    jQuery.ajax({
+		        url:     'loadrecalls.php', 
+		        type:     "GET", 
+		        dataType: "html", 
+		        data: {method: 'load_recalls'},  
+		        success: function(response) { 
+		        	$('#recalls').html(response);
+		    	},
+		    	error: function(response) { 
+		    		$('#recalls').html('<center style="margin: 50px auto 50px auto;"><b>There was an error loading recalls.</b><br /><button class="otziv" style="width:175px; height:50px;font-size:15px;margin-top: 3px;margin-bottom: 6px; margin-left:11px;" onclick="load_recalls()">Reload.</button></center>');
+		    	}
+		 	});
+		}
+		function load_page(target)
+		{
+			if (!$(target).hasClass('swchItemA'))
+			{			
+				$('.swchItemA').attr('class', 'swchItem');
+				let page_id = $(target).attr('page');
+				jQuery.ajax({
+					url:     'loadrecalls.php', 
+					type:     "GET", 
+					dataType: "html", 
+					data: {method: 'load_page', page_id: page_id},  
+					success: function(response) { 
+						$('#recall_page').html(response);
+					},
+					error: function(response) { 
+						$('#recall_page').html('<center style="margin: 50px auto 50px auto;"><b>There was an error loading recalls.</b><br /><button class="otziv" style="width:175px; height:50px;font-size:15px;margin-top: 3px;margin-bottom: 6px; margin-left:11px;" onclick="load_recalls()">Reload.</button></center>');
+					}
+				});
+				$('span[page="'+ page_id +'"]').attr('class', 'swchItemA');
+				$('html, body').animate({ scrollTop: $('#recalls').offset().top-5 }, 500);
+			}
+		}
+		$( document ).ready(function() {
+			load_recalls();
+		});
+	</script>
+	<div id="recalls">
+	</div>
+</body>
+</html>
